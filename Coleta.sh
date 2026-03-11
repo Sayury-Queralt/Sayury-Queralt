ac09e1240011eb90ed3b0a551c1a4453593bcc86
Sayury <fatimasayury@get.inatel.br>
2026-03-10 22:42:58 -0300

    Fix date format in Coleta.sh

--- Coleta.sh
+++ Coleta.sh
@@ -2,6 +2,6 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 while true; do
     hostname > "$DIR/hostname.txt"
-    date +"%%d/%m/%Y H:%M:%S" > "$DIR/hora.txt"
+    date +"%d/%m/%Y %H:%M:%S" > "$DIR/hora.txt"
     sleep 1
 done
